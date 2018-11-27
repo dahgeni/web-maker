5b1765dbffd5ab67395e67e5826a4112739f6f2d
TSN Student <student@ulem.org>
2018-11-27 10:54:46 -0500

    fixed errors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
+
   require("./services/user.service.server")(app);  
   require("./services/website.service.server")(app);
-  require("./services/pages.service.server")(app);
-  require (".services/widget.service.server")(app);
+  require("./services/page.service.server")(app);
+  require(".services/widget.service.server")(app);
 };
